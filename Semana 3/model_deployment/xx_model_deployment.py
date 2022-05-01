8655c7fbb3489f6f46993c9b81b1689059252b14
carlitoscanas <c.canas@uniandes.edu.co>
2022-05-01 14:51:54 -0600

    Add files via upload

---
+++ Semana 3/model_deployment/xx_model_deployment.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import pandas as pd
+import numpy as np
+import joblib
+import sys
+import os
+
+def pred_car_price(year, mileage, state, make, model):
+
+    # Features
+    car_info = [(year, mileage, state, make, model)]
+    car_info = pd.DataFrame(car_info, columns=['Year', 'Mileage', 'State', 'Make', 'Model'])#.to_numpy()
+    
+    # Model train
+    regressor = joblib.load(os.path.dirname(__file__) + '/car_price_final.pkl') 
+
+    # Make prediction
+    p1 = regressor.predict(car_info)
+
+    return p1
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 1:
+        print('Please insert a car info')
+    else:
+        car_info = sys.argv[1]
+        p1 = pred_car_price(car_info)
+        print(car_info)
+        print('Car Price: ', p1)
